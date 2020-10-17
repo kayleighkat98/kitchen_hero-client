caba14d2cb5377558bf10869f8e765372fd52f99
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-16 23:22:07 -0500

    private route

---
+++ src/utils/PrivateRoute.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import IngredientContext from '../contexts/IngredientContext'
+import UserContext from '../contexts/UserContext'
+
+
+export default function PrivateRoute({ component, ...props }) {
+  const Component = component
+  return (
+    <Route
+      {...props}
+      render={componentProps => (
+        <UserContext.Consumer>
+          {userContext => <IngredientContext.Consumer>
+            {ingredientContext =>
+              !!userContext.user.id
+                ? <Component {...componentProps} />
+                : (
+                  <Redirect
+                    to={{
+                      pathname: userContext.user.idle ? '/login' : '/register',
+                      state: { from: componentProps.location },
+                    }}
+                  />
+                  )
+            }
+          </IngredientContext.Consumer>}
+
+        </UserContext.Consumer>
+      )}
+    />
+  )
+}
