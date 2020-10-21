6738b30eb758929e789eafa65be106edff3ae3b5
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-21 12:33:08 -0500

    reformat header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,62 @@
 import React, { Component } from 'react'
-import './Header.css';
-//import  from '../';
+import './Header.css'
+import { Link } from 'react-router-dom'
+import TokenService from '../../services/token-service'
+import UserContext from '../../contexts/UserContext'
 
 class Header extends Component {
+  static contextType = UserContext
 
+  handleLogoutClick = () => {
+    this.context.processLogout()
+  }
+  renderLogoutLink() {
+    return (
+      <div>
+        <nav>
+          <Link
+            className='nav-item'
+            onClick={this.handleLogoutClick}
+            to='/login'>
+            Logout
+          </Link>
+          {' '}
+          <Link to='/about'>About</Link>
+          <span className='nav-item'>
+            {this.context.user.name}
+          </span>
+        </nav>
+      </div>
+    )
+  }
+  renderLoginLink() {
+    return (
+      <nav>
+        <Link to='/login' className='nav-item'>Login</Link>
+        {' '}
+        <Link to='/register' className='nav-item'>Sign up</Link>
+        {' '}
+        <Link to='/about'>About</Link>
+      </nav>
+    )
+  }
 
-    render() {
-        return(
-            <div className='header centerText'>
-               <h1>Kitchen-Hero</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <header className="header-container">
+        <h1 className="header center">
+          <Link to='/'>
+          Kitchen-Hero
+          </Link>
+        </h1>
+        <div className='nav'>
+          {TokenService.hasAuthToken()
+            ? this.renderLogoutLink()
+            : this.renderLoginLink()}
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header
